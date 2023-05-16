39137d25d06bfa2c208cef59f47af0db3aca3479
wert2all <wert2all@gmail.com>
2023-05-16 16:55:05 +0300

    feat: add angular environments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './pages/index/index-page.component';
 import { BlockElementComponent } from './components/block-element/block-element.component';
+import { Environment } from './types/environment';
+import { environment } from 'src/environments/environment';
+
+export const ENV = new InjectionToken<Environment>('env');
 
 @NgModule({
   declarations: [AppComponent, IndexPageComponent, BlockElementComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [{ provide: ENV, useFactory: () => environment }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { Environment } from 'src/app/types/environment';
+
+export const environment: Environment = {
+  production: true,
+  apiPath: 'https://wert2all.nsupdate.info/api/v4/',
+};
