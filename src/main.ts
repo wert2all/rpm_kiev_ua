343ca4f016d062cb71ccb05afc4cb872fac5f868
wert2all <wert2all@gmail.com>
2023-05-22 18:13:25 +0300

    feat: add sentry support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import * as Sentry from '@sentry/angular-ivy';
+import { environment } from 'src/environments/environment';
+Sentry.init({
+  dsn: environment.sentryDsn,
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
